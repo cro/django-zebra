68a697e64130588f155b8b6e997dce1e00e814c2
C. R. Oldham <cro@ncbt.org>
2012-05-11 21:21:45 -0600

    Enable zebra templatetags to work with a form that is being used inside a wizard.

--- zebra/templatetags/zebra_tags.py
+++ zebra/templatetags/zebra_tags.py
@@ -9,16 +9,18 @@ from zebra.conf import options
 
 
 register = template.Library()
-
 def _set_up_zebra_form(context):
     if not "zebra_form" in context:
         if "form" in context:
             context["zebra_form"] = context["form"]
+        elif "form" in context["wizard"]:
+            context["zebra_form"] = context["wizard"]["form"]
         else:
             raise Exception, "Missing stripe form."
-    context["STRIPE_PUBLISHABLE"] = options.STRIPE_PUBLISHABLE
-    return context
 
+    context["STRIPE_PUBLISHABLE"] = options.STRIPE_PUBLISHABLE
+
+    return context
 
 @register.inclusion_tag('zebra/_stripe_js_and_set_stripe_key.html', takes_context=True)
 def zebra_head_and_stripe_key(context):
